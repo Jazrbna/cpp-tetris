f4f50b8c1da9a914b92af89e1a4cb7e6ad0e9b16
Jaz <jasminefordbr@gmail.com>
2026-04-16 14:44:18 +0100

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
-#include <iostream>
 #include "tetris.h"
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+using namespace std;
 
 int main() {
-    std::cout << "Tetris project starting..." << std::endl;
+    Tetris game;
+
+    while (true) {
+        game.draw();
+        game.update();
+
+        // simple automatic delay
+        this_thread::sleep_for(chrono::milliseconds(300));
+    }
+
     return 0;
 }
